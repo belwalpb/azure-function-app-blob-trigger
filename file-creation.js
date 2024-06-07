c17645b7adf1b860545dfc6eca8125fe1c7bfc97
Priyanshu Belwal <priyanshubelwalpb@gmail.com>
2024-06-07 22:01:46 +0530

    Modify: File Syncup

--- file-creation.js
+++ file-creation.js
@@ -10,7 +10,15 @@ const filePath = path.join(__dirname, fileName);
 const start=1;
 const end= 10000;
 
-for(let i=start; i<=end; i++){
-    const data = randomBytes(100).toString('hex');
-    fs.appendFileSync(filePath, data + '\n');
+// Start and end dates
+const startDate = new Date('2016-01-01');
+const endDate = new Date('2024-06-01');
+
+for(let i = start; i <= end; i++){
+    for(let d = new Date(startDate); d <= endDate; d.setMonth(d.getMonth() + 1)){
+        const year = d.getFullYear();
+        const month = String(d.getMonth() + 1).padStart(2, '0'); // Months are 0-based in JavaScript
+        const date = `${i}-${year}-${month}-01\n`;
+        fs.appendFileSync(filePath, date);
+    }
 }
